682b99721c17d45afa1c6491d719f7dc00791f99
Nestor Carvacho <157805860+NCarvacho@users.noreply.github.com>
2024-02-05 12:13:21 -0300

    actualizacion web

--- sth/ecommerce/admin.py
+++ sth/ecommerce/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
+admin.site.register(Categoria)
+admin.site.register(Comuna)
+admin.site.register(Cliente)
+admin.site.register(Entrega)
+admin.site.register(EstadoEntrega)
+admin.site.register(Laboratorio)
 admin.site.register(Marca)
 admin.site.register(Modelo)
+admin.site.register(Pedido)
 admin.site.register(Producto)
+admin.site.register(ProductoPedido)
+admin.site.register(Region)
+admin.site.register(TipoPago)
+admin.site.register(Transporte)
+admin.site.register(Venta)
--- sth/ecommerce/models.py
+++ sth/ecommerce/models.py
@@ -1,38 +1,110 @@
+import datetime
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here. 
+class Categoria(models.Model):
+    nombrecategoria = models.CharField(max_length=80)
+        
+    def __str__(self):
+        return self.nombrecategoria
+
+
+class Comuna(models.Model):
+    nombrecomuna = models.CharField(max_length=80)
+    region = models.ForeignKey('Region', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombrecomuna
+
+class Cliente(models.Model):
+    pnombrecliente = models.CharField(max_length=80)
+    snombrecliente = models.CharField(max_length=80, null=True, blank=True)
+    appaternocliente = models.CharField(max_length=80)
+    apmaternocliente = models.CharField(max_length=80)
+    direccioncliente = models.CharField(max_length=250)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.pnombrecliente,' ',self.appaternocliente,' ', self.apmaternocliente)
+
+class Entrega(models.Model):
+    idpedido = models.ForeignKey('Pedido', on_delete=models.CASCADE)
+    idestadoentrega = models.ForeignKey('EstadoEntrega', on_delete=models.CASCADE)
+    idtransporte = models.ForeignKey('Transporte', on_delete=models.CASCADE)
+    
+
+class EstadoEntrega(models.Model):
+    nombreestado = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.nombreestado
+
+class Laboratorio(models.Model):
+    nombreLaboratorio = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.nombreLaboratorio
+
 class Marca(models.Model):
-    idMarca = models.AutoField(primary_key=True,
-                                verbose_name='id Marca')
-    nombreMarca = models.CharField(max_length=80,
-                                   null=False,
-                                   verbose_name='Nombre Marca')
+    nombreMarca = models.CharField(max_length=80)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.nombreMarca
 
 class Modelo(models.Model):
-    idModelo = models.AutoField(primary_key=True,
-                                verbose_name='id Modelo')
-    nombreModelo = models.CharField(max_length=80,
-                                   null=False,
-                                   verbose_name='Nombre Modelo')
-    def __str__(self):
-        return self.nombreModelo
+    nombremodelo = models.CharField(max_length=80)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.nombremodelo 
+
 class Producto(models.Model):
-    idProducto = models.AutoField(primary_key=True,
-                                     verbose_name='id producto')
-    nombreProducto = models.CharField(max_length=80,
-                              verbose_name='Nombre Producto')
-    descripcionProducto = models.CharField(max_length=80,
-                              verbose_name='Descripcion Producto')
-    Precio = models.IntegerField(verbose_name='Precio')
-    
-    foto = models.ImageField(upload_to='core/img/', default='core/img/logo.png')
-
-    modelo = models.ForeignKey(Modelo ,on_delete=models.CASCADE)    
-
-    marca = models.ForeignKey(Marca ,on_delete=models.CASCADE)
+    nombreproducto = models.CharField(max_length=80)
+    numeroserieproducto = models.CharField(max_length=150)
+    fechaingresoproducto = models.DateTimeField(default=now, editable=False)
+    preciounitarioproducto = models.DecimalField(max_digits=9, decimal_places=2)
+    imagenproducto = models.ImageField(null=True, blank=True)
+    descripcionproducto = models.CharField(max_length=250)
+    productoactivo = models.IntegerField()
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.nombreProducto
+        return self.nombreproducto
+
+class Region(models.Model):
+    nombreregion = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.nombreregion
+
+class TipoPago(models.Model):
+    nombretipopago = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.nombretipopago
+
+class Transporte(models.Model):
+    nombretransporte = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.nombretransporte
+
+class Venta(models.Model):
+    fechaventa = models.DateTimeField(default=now, editable=False)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    tipopago = models.ForeignKey(TipoPago, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+class Pedido(models.Model):
+    direccionentregapedido = models.CharField(max_length=250)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    
+class ProductoPedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
